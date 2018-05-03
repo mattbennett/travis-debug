abf2b9a3375a4a733d038aa76ddae1484d7755aa
Matt Bennett <matt@bennett.name>
2018-05-03 16:36:38 +0100

    add deployment to test.pypi.org

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='travis_debug',
+    version='0.0.1',
+    install_requires=[],
+    zip_safe=True,
+    license='Apache License, Version 2.0',
+    classifiers=[
+        "Programming Language :: Python",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+    ]
+)
